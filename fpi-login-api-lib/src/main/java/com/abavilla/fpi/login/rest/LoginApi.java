63303a78d1b656e5d8313a9c4fe13162711cfd57
Vince Jerald Villamora <vincevillamora@gmail.com>
2022-10-10 02:01:30 +0800

    Add LoginApi

---
+++ fpi-login-api-lib/src/main/java/com/abavilla/fpi/login/rest/LoginApi.java
@@ -0,0 +1,53 @@
+/*
+ * *****************************************************************************
+ *  * FPI Application - Abavilla                                                 *
+ *  * Copyright (C) 2022  Vince Jerald Villamora                                 *
+ *  *                                                                            *
+ *  * This program is free software: you can redistribute it and/or modify       *
+ *  * it under the terms of the GNU General Public License as published by       *
+ *  * the Free Software Foundation, either version 3 of the License, or          *
+ *  * (at your option) any later version.                                        *
+ *  *                                                                            *
+ *  * This program is distributed in the hope that it will be useful,            *
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ *  * GNU General Public License for more details.                               *
+ *  *                                                                            *
+ *  * You should have received a copy of the GNU General Public License          *
+ *  * along with this program.  If not, see <https://www.gnu.org/licenses/>.     *
+ *  *****************************************************************************
+ */
+
+package com.abavilla.fpi.login.rest;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import com.abavilla.fpi.login.dto.LoginDto;
+import com.abavilla.fpi.login.dto.SessionDto;
+import com.abavilla.fpi.login.dto.WebhookLoginDto;
+import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.reactive.RestResponse;
+
+@RegisterRestClient(configKey = "login-api")
+public interface LoginApi {
+
+  /**
+   * Obtain a session token from authentication service.
+   * @param login {@link LoginDto} object
+   *
+   * @return {@link SessionDto} object containing the session info
+   */
+  @POST
+  Uni<SessionDto> authenticate(LoginDto login);
+
+  /**
+   * Perform a trusted login
+   * @param login Login credentials
+   * @return
+   */
+  @POST
+  @Path("trusted")
+  Uni<RestResponse<String>> webhookAuthenticate(WebhookLoginDto login);
+}
